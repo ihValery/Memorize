08cca75ae3435dad2527247d763f85a44e633888
Valery <ihValery@gmail.com>
2021-04-18 21:36:08 +0300

    Медленно, но настойчиво погружаемся в декларативное описание UI, которое проповедует SwiftUI познакомились: var body: some View ZStack ForEach HStack

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    //Свойство var с именем body и ТИПОМ some View ещё интересна тем, является вычисляемой (computed)
+    // Свойство var с именем body и ТИПОМ some View ещё интересна тем, является вычисляемой (computed)
     var body: some View {
-        return ZStack(content: {
-            RoundedRectangle(cornerRadius: 13.0).stroke()
-            Text("👻")
-        }).padding()
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        // функция font воздействует ТОЛЬКО на ВСЕ Text, входящие в стек ZStack.
+        .font(.largeTitle)
     }
 }
 
-//Обеспечивает связь между кодом, и областью справа, которая называется Preview.
+// Структура для одной карты
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 13.0)
+                    // Есть необязательный аргумент и этим аргументом является цвет Color
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 13.0)
+                    // Могут быть другие аргументы, и один из них — ширина линии обводки lineWdth.
+                    .stroke(lineWidth: 3.0)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 13.0).fill()
+            }
+        }
+    }
+}
+
+// Обеспечивает связь между кодом, и областью справа, которая называется Preview.
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
