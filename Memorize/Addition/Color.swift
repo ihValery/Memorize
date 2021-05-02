74d3e64844443d40129a17272ff2251e14e0ef50
Valery <ihValery@gmail.com>
2021-05-03 01:41:38 +0300

    ДЗ 3 пункта пока

---
+++ Memorize/Addition/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  Memorize
+//
+//  Created by Валерий Игнатьев on 2.05.21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let lightScheme = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
+    static let darkScheme = Color(red: 25 / 255, green: 25 / 255, blue: 30 / 255)
+    static var colorText: Color {
+        Color(UIColor { $0.userInterfaceStyle == .dark ? .black : .white } )
+    }
+
+//    static func c3(for colorScheme: ColorScheme) -> Color {
+//        colorScheme == .dark ? .black : .white
+//    }
+}
