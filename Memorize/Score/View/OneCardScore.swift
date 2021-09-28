eeada34262d969b5c83c326aa7ecd474f696392f
ihValery <ihValery@gmail.com>
2021-09-28 06:48:20 +0300

    ref

--- Memorize/Score/View/OneCardScore.swift
+++ Memorize/Score/View/OneCardScore.swift
@@ -15,47 +15,66 @@ struct OneCardScore: View {
     var body: some View {
         ZStack {
             HStack {
-                Text(scoreViewModel.score.theme)
-                    .font(.system(size: 60))
-                
-                VStack(alignment: .leading) {
-                    Text("Результат  ")
-                        .font(.title)
-                    Text(scoreViewModel.dateToString())
-                        .font(.subheadline).opacity(0.6)
-                }
-                .opacity(0.6)
                 Spacer()
+                Image(systemName: "trash")
+                    .font(.largeTitle)
+                    .foregroundColor(.red)
+                    .offset(x: 40)
+                    .offset(x: viewState.width)
+//                    .padding(.trailing, 50)
+//                    .opacity(viewState.width < 40 ? 1 : 0)
             }
             
-            HStack {
-                Spacer()
-                Text(String(scoreViewModel.score.maxScore))
-                    .font(.system(size: 50).bold())
-                    .opacity(0.7)
-            }
-        }
-        .frame(maxWidth: .infinity)
-        .padding(.horizontal, 40)
-        .background(BgCardScore())
-        .padding(.vertical, 35).padding(.leading, 10)
-        
-        .gesture(
-            DragGesture()
-                .onChanged { value in
-                    viewState = value.translation
-                }
-                .onEnded { value in
-                    if value.location.x < value.startLocation.x - 40 {
-                        showAlert.toggle()
+            ZStack {
+                HStack {
+                    Text(scoreViewModel.score.theme)
+                        .font(.system(size: withBangs() ? 60 : 50))
+                    
+                    VStack(alignment: .leading) {
+                        Text("Результат  ")
+                            .font(withBangs() ? .title : .title2)
+                        Text(scoreViewModel.dateToString())
+                            .font(withBangs() ? .subheadline : .footnote).opacity(0.6)
                     }
-                    viewState = .zero
-                })
-        
-        .alert(isPresented: $showAlert) {
-            Alert(title: Text("Удалить счет"), message: Text("Это действие нельзя будет отменить"),
-                  primaryButton: .destructive(Text("Удалить")) { scoreViewModel.remove() },
-                  secondaryButton: .cancel(Text("Нет")))
+                    .opacity(0.6)
+                    Spacer()
+                }
+                
+                HStack {
+                    Spacer()
+                    Text(String(scoreViewModel.score.maxScore))
+                        .font(.system(size: withBangs() ? 50 : 40).bold())
+                        .opacity(0.7)
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal, 40)
+            .background(BgCardScore())
+            .padding(.vertical, 35).padding(.leading, 10)
+            .offset(x: viewState.width)
+            
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        if value.translation.width < 0 {
+                            viewState = value.translation
+                        } else {
+                            viewState = .zero
+                        }
+                    }
+                    .onEnded { value in
+                        if value.location.x < value.startLocation.x - 50 {
+                            showAlert.toggle()
+                        }
+                        viewState = .zero
+                    }
+            )
+            
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Удалить счет"), message: Text("Это действие нельзя будет отменить"),
+                      primaryButton: .destructive(Text("Удалить")) { scoreViewModel.remove() },
+                      secondaryButton: .cancel(Text("Нет")))
+        }
         }
     }
 }
