fb5e76308eff42fbb08ce25252db829041f8f917
Valery <ihValery@icloud.com>
2021-12-29 06:04:13 +0300

    Up

--- Memorize/Helper/CustomCorners.swift
+++ Memorize/Helper/CustomCorners.swift
@@ -13,7 +13,9 @@ struct CustomCorners: Shape {
     var radius: CGFloat
     
     func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corner, cornerRadii: CGSize(width: radius, height: radius))
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: corner,
+                                cornerRadii: CGSize(width: radius, height: radius))
         
         return Path(path.cgPath)
     }
